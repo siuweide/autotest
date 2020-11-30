1fb88c63ec89840f91846031327541ac7b8fe111
siuweide <328527390@qq.com>
2020-11-30 18:36:46 +0800

    更新

--- project/urls.py
+++ project/urls.py
@@ -3,5 +3,7 @@ from project.views import project_views
 
 urlpatterns = [
     # 项目列表
-    path('list/', project_views.list)
+    path('list/', project_views.list),
+    # 添加项目
+    path('add/', project_views.add),
 ]
--- project/views/project_views.py
+++ project/views/project_views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from project.models import Project
+from django.http import JsonResponse
 
 # 项目列表
 def list(request):
@@ -7,3 +8,17 @@ def list(request):
     return render(request, 'project/list.html', {
         'pj_list': pj_list
     })
+
+# 创建项目
+def add(request):
+    name = request.POST.get('name', '')
+    describe = request.POST.get('describe', '')
+    status = request.POST.get('status', '')
+    if status == 'true':
+        real_status = True
+
+    Project.objects.create(name=name, describe=describe, status=real_status)
+
+    return JsonResponse({"code": 200, "msg": "success"})
+
+
