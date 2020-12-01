805ed86c24d1f3f9d49895f071fc5442cc4c36af
siuweide <328527390@qq.com>
2020-12-01 22:34:46 +0800

    上传

--- project/forms.py
+++ project/forms.py
@@ -1,12 +1,14 @@
 from django import forms
+from django.forms import widgets
 
 class AddProjectForm(forms.Form):
-    name = forms.CharField(label='项目名称', widget=forms.TextInput(attrs={
+    name = forms.CharField(label='项目名称', widget=widgets.TextInput(attrs={
         'class': 'form-control'
     }))
-    describe = forms.CharField(label='描述', widget=forms.Textarea(attrs={
+    describe = forms.CharField(label='描述', widget=widgets.Textarea(attrs={
         'class': 'form-control'
     }))
-    status = forms.BooleanField(label='状态', widget=forms.CheckboxSelectMultiple(attrs={
-        'class': "custom-control-label custom-control-color"
+    status = forms.BooleanField(label='状态', widget=widgets.CheckboxInput(attrs={
+        'id': 'check2',
+        'class': 'custom-control-input'
     }))
--- project/views/project_views.py
+++ project/views/project_views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from project.models import Project
 from project.forms import AddProjectForm
@@ -13,10 +13,22 @@ def list(request):
 # 创建项目
 def add(request):
     if request.method == 'GET':
+        print('进入get请求')
         form = AddProjectForm()
         return render(request, 'project/add.html', {
             'form':  form
         })
+    elif request.method == 'POST':
+        print('进入post请求')
+        form = AddProjectForm(request.POST)
+        if form.is_valid():
+            print('表单验证成功')
+            name = form.cleaned_data['name']
+            describe = form.cleaned_data['describe']
+            status = form.cleaned_data['status']
+
+            Project.objects.create(name=name, describe=describe, status=status)
+            return redirect('/project/list/')
 
 # 修改项目
 # def update(request, id):
